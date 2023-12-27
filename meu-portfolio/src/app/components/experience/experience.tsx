d3a6efe0ee32d9a87ff7b19bd346a5209999074c
Araujomann <lucastheeyepatch@gmail.com>
2023-12-27 17:47:53 -0300

    feat: first part finished

--- meu-portfolio/src/app/components/experience/experience.tsx
+++ meu-portfolio/src/app/components/experience/experience.tsx
@@ -1,5 +1,6 @@
 import { SectionTitle } from "../sectionTitle/section-title"
 import "./experience.scss"
+import Image from "next/image"
 
 export function Experience(){
     return(
@@ -7,7 +8,48 @@ export function Experience(){
         <SectionTitle text="Experience" />
         <p>Studying software development as a front-end and searching the firsts opportunities.</p>
         <div className="experience-time">
-
+          <div className="experience-language">
+          <Image
+              src="js 2.svg"
+              alt="javascript logo"
+              width={40}
+              height={40}
+              priority
+                />
+            <div className="experience-unit">
+              <div className="experience-measure measure-1">
+                <span>1 year</span>
+              </div>
+            </div>
+          </div>
+          <div className="experience-language">
+          <Image
+              src="ts 2.svg"
+              alt="typescript logo"
+              width={40}
+              height={40}
+              priority
+                />
+              <div className="experience-unit">
+                <div className="experience-measure measure-1">
+                  <span>1 year</span>
+                </div>
+              </div>
+          </div>
+          <div className="experience-language">
+          <Image
+              src="react 2.svg"
+              alt="react logo"
+              width={40}
+              height={40}
+              priority
+                />
+              <div className="experience-unit">
+                <div className="experience-measure measure-1">
+                  <span>1 year</span>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
         
--- meu-portfolio/src/app/components/social-btns/social-btn.tsx
+++ meu-portfolio/src/app/components/social-btns/social-btn.tsx
@@ -7,16 +7,16 @@ import "./social-btns.scss"
 export function SocialBtns(){
     return(
         <div className="social">
-            <a href="https://www.instagram.com/lucamzg/">
+            <a href="https://www.instagram.com/lucamzg/" target="_blank">
                 <InstaIcon/>
             </a>
-            <a href="https://www.linkedin.com/in/lucas-ara%C3%BAjo-4b3316226/">
+            <a href="https://www.linkedin.com/in/lucas-ara%C3%BAjo-4b3316226/" target="_blank">
                 <LinkedinIcon/>
             </a>
-            <a href="https://github.com/Araujomann">
+            <a href="https://github.com/Araujomann" target="_blank">
                 <GithubIcon/>
             </a>
-            <a href="https://twitter.com/theoldlusca">
+            <a href="https://twitter.com/theoldlusca" target="_blank">
                 <TwitterIcon/>
             </a>
         </div>
